6069a095eec0ccc3ef4003bc55f9ebebb120043b
aden <tutmoihlam@yandex.ru>
2020-07-20 00:15:36 +0300

    admin templates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,22 +1,18 @@
-@extends('layouts.app')
+@extends('admin.layouts.default')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        admin panel
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
-                </div>
+                @endif
+
+                admin panel
             </div>
         </div>
     </div>
---
+++ resources/views/admin/parts/sidemenu.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">Admin menu</div>
+        <div class="card-body">
+            <ul>
+                <li>
+                    <a href="{{route('product.index')}}">Products</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
